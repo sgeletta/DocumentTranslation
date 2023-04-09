e13130affdb59102c5edc7498911216160c31d75
Keyur S <skeyur@kdsol.com>
2023-04-09 09:04:09 -0500

    Created two setup (installer) projects Wix-3.11.2

---
+++ TextTranslation.Setup/ZipItUp.cmd
@@ -0,0 +1,12 @@
+del bin\release\DocumentTranslation.zip
+md temp
+del temp\*.* /s /f /q
+
+xcopy /q ..\DocumentTranslation.GUI\bin\Release\net6.0-windows\*.* temp
+xcopy /q /y ..\DocumentTranslation.CLI\bin\Release\net6.0\*.* temp
+
+tar.exe -a -c -p -f bin\release\DocumentTranslation.zip -C temp *.*
+pause
+
+del temp\*.* /s /f /q
+rd temp
