44acc543e8078e12a153ce8fa7e90b1837c81801
Keyur S <skeyur@kdsol.com>
2023-04-09 09:07:18 -0500

    Upgrading Wix from 3.11.2 -> 4.0

--- TextTranslation.Setup/ZipItUp.cmd
+++ TextTranslation.Setup/ZipItUp.cmd
@@ -1,11 +1,11 @@
-del bin\release\DocumentTranslation.zip
+del bin\release\TextTranslation.zip
 md temp
 del temp\*.* /s /f /q
 
-xcopy /q ..\DocumentTranslation.GUI\bin\Release\net6.0-windows\*.* temp
-xcopy /q /y ..\DocumentTranslation.CLI\bin\Release\net6.0\*.* temp
+xcopy /q ..\TextTranslation.GUI\bin\Release\net6.0-windows\*.* temp
+xcopy /q /y ..\TextTranslation.CLI\bin\Release\net6.0\*.* temp
 
-tar.exe -a -c -p -f bin\release\DocumentTranslation.zip -C temp *.*
+tar.exe -a -c -p -f bin\release\TextTranslation.zip -C temp *.*
 pause
 
 del temp\*.* /s /f /q
